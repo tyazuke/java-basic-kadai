52b21cd07b1c9d332302f959fc58ec24fb1a53e8
tyazuke <144684946+tyazuke@users.noreply.github.com>
2023-11-06 18:17:29 +0900

    Add files via upload

---
+++ kadai_004/Score_Chapter04.java
@@ -0,0 +1,20 @@
+package kadai_004;
+
+public class Score_Chapter04 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("A"+"さんの点数は"+10+"点");
+		System.out.println("B"+"さんの点数は"+20+"点");
+		System.out.println("C"+"さんの点数は"+30+"点");
+		System.out.println("D"+"さんの点数は"+40+"点");
+		System.out.println("E"+"さんの点数は"+50+"点");
+		System.out.println("F"+"さんの点数は"+60+"点");
+		System.out.println("G"+"さんの点数は"+70+"点");
+		System.out.println("H"+"さんの点数は"+80+"点");
+		System.out.println("I"+"さんの点数は"+90+"点");
+		System.out.println("J"+"さんの点数は"+100+"点");
+		System.out.println("テストの平均点は"+((10+20+30+40+50+60+70+80+90+100)/10)+"点");
+	}
+
+}
