052add247a93aae8d74b3823d755f69020f3f378
tyazuke <144684946+tyazuke@users.noreply.github.com>
2023-11-18 17:08:20 +0900

    Add files via upload

---
+++ kadai_014/CarExec_Chapter14.java
@@ -0,0 +1,13 @@
+package kadai_014;
+
+public class CarExec_Chapter14 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		Car_Chapter14 gear = new Car_Chapter14();
+		gear.gearChange(3);
+		gear.run();
+		
+	}
+
+}
